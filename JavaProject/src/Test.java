e4f4bb7cae4aec7754da0e306f07de823bad2177
bvashwani1 <ashwani.saxena@in.bureauveritas.com>
2018-05-07 15:14:31 +0530

    test2

--- JavaProject/src/Test.java
+++ JavaProject/src/Test.java
@@ -1,6 +1,16 @@
 
 public class Test {
 	public static void main(String[] args) {
+		test();
+	}
+
+	private static void test() {
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
+		System.out.println("Hello World");
 		System.out.println("Hello World");
 	}
+
+
 }
